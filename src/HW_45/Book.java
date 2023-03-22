8a5c976ada63ab4ae28cf05254f6fa1a13bf9708
Dina Migunova <migunova.dina@gmail.com>
2023-03-22 11:39:52 +0100

    Add method readBook();

--- src/HW_45/Book.java
+++ src/HW_45/Book.java
@@ -8,6 +8,10 @@ package HW_45;
 // а если авторы совпадают - по названиям
 // (и там, и там - по алфавиту, "в словарном порядке").
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Book {
     private String author;
 
@@ -19,6 +23,19 @@ public class Book {
         this.author = author;
         this.title = title;
         this.numberPages = numberPages;
+    }
+
+    public static Book readBook() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter book`s author: ");
+        String author = br.readLine();
+        System.out.print("Enter book`s title: ");
+        String title = br.readLine();
+        System.out.print("Enter number of pages: ");
+        int numberPages = Integer.parseInt(br.readLine());
+
+        return new Book(author, title, numberPages);
     }
 
 
